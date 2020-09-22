a13467b18725c48332a7a917967782f5bd64afbc
Carter MacLennan <carter.maclennan@verosource.com>
2020-09-21 22:19:24 -0300

    Updates

---
+++ precache-manifest.f825ac1ca71928132a5dc944fdf799f7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4980514e1952c384d7c3c1fa006e6961",
+    "url": "/MyWebsite/index.html"
+  },
+  {
+    "revision": "24eb054338ee5a915e67",
+    "url": "/MyWebsite/static/css/main.aeb7999c.chunk.css"
+  },
+  {
+    "revision": "e09dd93b620738c2239a",
+    "url": "/MyWebsite/static/js/2.da7c28d1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/MyWebsite/static/js/2.da7c28d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24eb054338ee5a915e67",
+    "url": "/MyWebsite/static/js/main.d8460c2b.chunk.js"
+  },
+  {
+    "revision": "671bd5fa3146e2a2892c",
+    "url": "/MyWebsite/static/js/runtime-main.f3cf8d64.js"
+  },
+  {
+    "revision": "6c5c149c6cbfff0dadee22a927d1920e",
+    "url": "/MyWebsite/static/media/LilConsistentMe.6c5c149c.png"
+  },
+  {
+    "revision": "b3a84680e72e28935c5517e87cecea79",
+    "url": "/MyWebsite/static/media/Resume.b3a84680.pdf"
+  },
+  {
+    "revision": "a10234919e5b7256751ebb22711145d0",
+    "url": "/MyWebsite/static/media/RunSmart.a1023491.png"
+  },
+  {
+    "revision": "0cd9748c88d8bd1459a64cb725156655",
+    "url": "/MyWebsite/static/media/profile.0cd9748c.png"
+  }
+]);
